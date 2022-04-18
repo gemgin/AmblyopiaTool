e3382e48ee466cfb909dfbed62cf79e03ea07d82
gemgin <23352428+gemgin@users.noreply.github.com>
2022-04-18 10:18:44 +0800

    资源本地化

--- doc/js/wza-m.max.js
+++ doc/js/wza-m.max.js
@@ -1,7 +1,7 @@
 var _wzat = _wzat || [];
 (function() {
 var wza = document.createElement("script");
-wza.src = "//wza.rrbay.com/js/v2.1/wzatool-m.max.js";
+wza.src = "//res.rrbay.com/js/v2.1/wzatool-m.max.js";
 var s = document.getElementsByTagName("script")[0];
 s.parentNode.insertBefore(wza, s);
 })();
